d52f48a69c3c5c5da2d0b29c3ad35f1d0b71bc65
Ray <csharp2002@hotmail.com>
2026-06-14 14:37:21 +0800

    Update SHA256 checksums for mindx binary

--- Formula/mindx.rb
+++ Formula/mindx.rb
@@ -19,12 +19,12 @@ class Mindx < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/DotNetAge/mindx/releases/download/v2.0.9/mindx-2.0.9-darwin-amd64.tar.gz"
-      sha256 "9031692aa8f5d7b79fba665a188e8d6eee3290d362a6e6e510094c268e49ae19"
+      sha256 "969167feec6d29cae92fadd7d1620074bd073b528384d0e8a8bc06ae63fc15af"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/DotNetAge/mindx/releases/download/v2.0.9/mindx-2.0.9-darwin-arm64.tar.gz"
-      sha256 "e4104a2a115caecf8b1fd894f6a27c5759f80357c44f3a3c063c86927574dcf7"
+      sha256 "5d813051811a1bb21f230c2c8274a146a5dc4890980a220ce753d7906fc5e78b"
     end
   end
 
