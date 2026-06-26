6ae4bb8165c028abc3f2e2ef55ad1b1987304a89
DotNetAge <DotNetAge@users.noreply.github.com>
2026-06-26 05:42:54 +0000

    mindx 2.3.3

--- Formula/mindx.rb
+++ Formula/mindx.rb
@@ -1,12 +1,12 @@
 # typed: false
 # frozen_string_literal: true
-# Template: DotNetAge/mindx/v2.3.2
+# Template: DotNetAge/mindx/v2.3.3
 # Placeholders (replaced by CI/release script):
 #   DotNetAge/mindx  → e.g. DotNetAge/mindx
-#   2.3.2      → e.g. 2.2.0
-#   v2.3.2          → e.g. v2.2.0
-#   0bbfd01b2fd019ee90327485bf70b1101bf5e9bc78b830da31a3f6747c1fbef3 → SHA256 of darwin-amd64 tarball
-#   07d7052a6b44e8eee26cdf0afb27217826111ef505006793329442e09664e8f8 → SHA256 of darwin-arm64 tarball
+#   2.3.3      → e.g. 2.2.0
+#   v2.3.3          → e.g. v2.2.0
+#   66c9907e1fb12f0c5eb1f6acc86885b2338c0771f4cec5294ef338d5e47022c4 → SHA256 of darwin-amd64 tarball
+#   d4aab6257c3d23c0f8509152bed0faf87b66ada57457823765697ada0f7dc948 → SHA256 of darwin-arm64 tarball
 #
 # Tap repo: https://github.com/DotNetAge/homebrew-mindx
 
@@ -14,17 +14,17 @@ class Mindx < Formula
   desc "MindX - AI-native multi-agent conversation platform"
   homepage "https://github.com/DotNetAge/mindx"
   license "MIT"
-  version "2.3.2"
+  version "2.3.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/DotNetAge/mindx/releases/download/v2.3.2/mindx-2.3.2-darwin-amd64.tar.gz"
-      sha256 "0bbfd01b2fd019ee90327485bf70b1101bf5e9bc78b830da31a3f6747c1fbef3"
+      url "https://github.com/DotNetAge/mindx/releases/download/v2.3.3/mindx-2.3.3-darwin-amd64.tar.gz"
+      sha256 "66c9907e1fb12f0c5eb1f6acc86885b2338c0771f4cec5294ef338d5e47022c4"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/DotNetAge/mindx/releases/download/v2.3.2/mindx-2.3.2-darwin-arm64.tar.gz"
-      sha256 "07d7052a6b44e8eee26cdf0afb27217826111ef505006793329442e09664e8f8"
+      url "https://github.com/DotNetAge/mindx/releases/download/v2.3.3/mindx-2.3.3-darwin-arm64.tar.gz"
+      sha256 "d4aab6257c3d23c0f8509152bed0faf87b66ada57457823765697ada0f7dc948"
     end
   end
 
