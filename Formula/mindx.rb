4c06d780b1c6034eb8117a0c4e1b95e063b2a673
DotNetAge <DotNetAge@users.noreply.github.com>
2026-09-05 18:11:08 +0000

    mindx 2.5.6

--- Formula/mindx.rb
+++ Formula/mindx.rb
@@ -1,12 +1,12 @@
 # typed: false
 # frozen_string_literal: true
-# Template: DotNetAge/mindx/v2.5.4
+# Template: DotNetAge/mindx/v2.5.6
 # Placeholders (replaced by CI/release script):
 #   DotNetAge/mindx  → e.g. DotNetAge/mindx
-#   2.5.4      → e.g. 2.2.0
-#   v2.5.4          → e.g. v2.2.0
-#   a3c000360619ae61b7c53cd84eb2aeef2992d88a89b052020a318f50806195b0 → SHA256 of darwin-amd64 tarball
-#   bc335faf60cbfc9a232d337155d8eebc01e8b037c0f144aefe60bf824435263e → SHA256 of darwin-arm64 tarball
+#   2.5.6      → e.g. 2.2.0
+#   v2.5.6          → e.g. v2.2.0
+#   49f1278b7323933ba56f1d7d497bd3f05656b894099023f9526a5ec5ad32d504 → SHA256 of darwin-amd64 tarball
+#   bf89977f25e16aa8822b31bb31cef537c0167a8d42820e606c754d715fecaed5 → SHA256 of darwin-arm64 tarball
 #
 # Tap repo: https://github.com/DotNetAge/homebrew-mindx
 
@@ -14,19 +14,19 @@ class Mindx < Formula
   desc "MindX - AI-native multi-agent conversation platform"
   homepage "https://github.com/DotNetAge/mindx"
   license "MIT"
-  version "2.5.4"
+  version "2.5.6"
 
   depends_on "onnxruntime"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/DotNetAge/mindx/releases/download/v2.5.4/mindx-2.5.4-darwin-amd64.tar.gz"
-      sha256 "a3c000360619ae61b7c53cd84eb2aeef2992d88a89b052020a318f50806195b0"
+      url "https://github.com/DotNetAge/mindx/releases/download/v2.5.6/mindx-2.5.6-darwin-amd64.tar.gz"
+      sha256 "49f1278b7323933ba56f1d7d497bd3f05656b894099023f9526a5ec5ad32d504"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/DotNetAge/mindx/releases/download/v2.5.4/mindx-2.5.4-darwin-arm64.tar.gz"
-      sha256 "bc335faf60cbfc9a232d337155d8eebc01e8b037c0f144aefe60bf824435263e"
+      url "https://github.com/DotNetAge/mindx/releases/download/v2.5.6/mindx-2.5.6-darwin-arm64.tar.gz"
+      sha256 "bf89977f25e16aa8822b31bb31cef537c0167a8d42820e606c754d715fecaed5"
     end
   end
 
